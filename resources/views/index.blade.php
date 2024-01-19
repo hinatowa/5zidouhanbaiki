9dac07cc97c49a50ee9390f0c312eba864ac877b
HINATOWA <3126iyuki@gmail.com>
2024-01-19 09:01:15 +0900

    インデント調整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,8 @@
             <div class="text-left">
                 <h2 style="font-size:1rem;">自動販売機</h2>
             </div>
-            <div>
                 <form action="" method="GET">
                 @csrf
-                
                     <input type="text" name="keyword">
                     <select name="companies_name" data-toggle="select">
                         <option value="">メーカー名</option>
@@ -21,33 +19,34 @@
                     <div class="price.search">
                         <div class="jougen">
                             <label for="jougen">価格上限</label>
-                                <input type="number" name="jougenpr" min="100" max="500">
+                            <input type="number" name="jougenpr" min="100" max="500">
                         </div>
 
                         <div class="kagen">
                             <label for="kagen">価格下限</label>
-                                <input type="number" name="kagenpr" min="100" max="500">
+                            <input type="number" name="kagenpr" min="100" max="500">
                         </div>
                     </div>
 
                     <div class="stock.search">
                         <div class="jougen">
                             <label for="jougen">在庫上限</label>
-                                <input type="number" name="jougenst" min="1" max="500">
+                            <input type="number" name="jougenst" min="1" max="500">
                         </div>
 
                         <div class="kagen">
                             <label for="kagen">在庫下限</label>
-                                <input type="number" name="kagenst" min="1" max="500">
+                             <input type="number" name="kagenst" min="1" max="500">
                         </div>
                     </div>
-                        <button id="btn" class="btn btn-secondary" >検索</button>
+
+                    <button id="btn" class="btn btn-secondary" >検索</button>
 
                 </form>
-                        <div class="text-right">
-                          <a class="btn btn-success" href="{{ route('product.create') }}">新規登録</a>
-                        </div>
-            </div>
+
+                    <div class="text-right">
+                        <a class="btn btn-success" href="{{ route('product.create') }}">新規登録</a>
+                    </div>
         
                     <table id="productTable" class="table table-bordered">
                         <tr>
@@ -61,11 +60,11 @@
                             <th>削除</th>
                         </tr>                   
                     </table>
-            </div>     
-        </div>             
+        </div>     
+    </div>             
 
                 <form action="" method="GET">
-                        @csrf
+                @csrf
                     <script>
                         $('#productTable th').on('click', function (){
                             const columnname=$(this).data("column");
@@ -86,10 +85,7 @@
                             return false;
                         });
 
-
-
                         function ajaxRequest(columnname,direction){
-
                             $('.product-list').empty();
                             const keyword = $('input[name="keyword"]').val();
                             const companies_name = $('select[name="companies_name"]').val();
@@ -98,10 +94,10 @@
                             const jougenst = $('input[name="jougenst"]').val();
                             const kagenst = $('input[name="kagenst"]').val();
                             $.ajax({
-                                    type: "get", //HTTP通信の種類
-                                    url: "/5zidouhanbaiki/public/product/getlistAjax",
-                                    dataType: "json",
-                                    data: {
+                                type: "get", //HTTP通信の種類
+                                url: "/5zidouhanbaiki/public/product/getlistAjax",
+                                dataType: "json",
+                                data: {
                                         keyword : keyword ,
                                         companies_name ,
                                         jougenpr , 
@@ -111,10 +107,9 @@
                                         sort: columnname,
                                         direction: direction,
                                     },
-                                })
+                            })
                                 //通信が成功したとき
-                                .done((res) => { // resの部分にコントローラーから返ってきた値 $users が入る
-                                    
+                                .done((res) => { // resの部分にコントローラーから返ってきた値 $users が入る                                    
                                     console.log("Ajax成功");
                                     console.log(companies_name);
                                     console.log(keyword);
@@ -136,7 +131,7 @@
                                                 <td class="col-xs-2"><button type="button" class="btn btn-danger" onclick='return confirm("削除しますか？");'>削除</button></td>
                                             </tr>                                        
                                                 `;
-                                         $("#productTable").append(html); //できあがったテンプレートを table table-borderedクラスの中に追加
+                                        $("#productTable").append(html); //できあがったテンプレートを table table-borderedクラスの中に追加
                                     });
                                 })
                                 //通信が失敗したとき
@@ -145,22 +140,11 @@
                                 console.log(error.statusText);
                                 console.log(error);
                                 });
-                                
                             return false;
-
                         }
-
-                        
-
-
-
-
-
-
-
                     </script>
 
-                </form>
+                </form>＋＋—
             
            
     {{ $products->links('vendor.pagination.bootstrap-4') }}
